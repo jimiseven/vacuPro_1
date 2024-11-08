60c82f0824f7234e3cb5d78b41f6de34d4b1d3a6
Jimi Joe <tano777864@gmail.com>
2024-11-08 12:04:19 -0400

    bd

--- bd/vacupro_2.sql
+++ bd/vacupro_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2024 a las 23:29:48
+-- Tiempo de generación: 08-11-2024 a las 17:03:47
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -50,8 +50,7 @@ CREATE TABLE `niños` (
 
 INSERT INTO `niños` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `fecha_nacimiento`, `numero_acta_nacimiento`, `numero_cedula_identidad`, `fecha_registro`, `nombre_responsable`, `apellido_paterno_tutor`, `apellido_materno_tutor`, `numero_cedula_tutor`, `relacion`, `telefono_tutor`) VALUES
 (1, 'jael', 'mons', 'mars', '2024-10-31', '234523452', '32452345', '2024-11-05 20:56:36', 'carlos', 'mons', 'mons', '6549874', 'Padre', '76992490'),
-(2, 'marta', 'mons', 'mons', '2024-11-01', '234523452345', '23452345', '2024-11-05 21:39:10', 'padre', 'madre', 'mons', '2341234', 'Madre', '123512351'),
-(3, 'paola', 'callejas', 'caceres', '2024-08-27', '24562457274', '34523452345', '2024-11-05 22:22:57', 'su mama', 'su papa', 'mos', '64564654', 'Padre', '76992490');
+(3, 'paola andrea', 'callejas', 'caceres', '2024-08-27', '12322', '34523452345', '2024-11-05 22:22:57', 'su mama', 'su papa', 'mos', '64564654', 'Padre', '76992490');
 
 -- --------------------------------------------------------
 
@@ -73,9 +72,7 @@ CREATE TABLE `personal` (
 --
 
 INSERT INTO `personal` (`id`, `puesto`, `nombre`, `apellido`, `numero_cedula_identidad`, `celular`) VALUES
-(1, 'Medico', 'jimi', 'mons', '423535|', '34634563456'),
-(2, 'Asistente', 'gino', 'mars', '46546544', NULL),
-(3, 'Medico', 'carlos', 'moars', '684798798', '964654654');
+(1, 'Enfermero', 'jimi', 'mons', '', '34634563456');
 
 -- --------------------------------------------------------
 
@@ -101,14 +98,16 @@ INSERT INTO `vacunaciones` (`id`, `id_nino`, `tipo_id`, `numero_dosis`, `fecha_a
 (2, 1, 3, 1, '2024-11-07', 1),
 (3, 1, 3, 1, '2024-11-07', 1),
 (4, 1, 4, 2, '2024-11-06', 1),
-(5, 2, 1, 1, '2024-11-02', 2),
-(6, 2, 1, 1, '2024-11-02', 2),
-(7, 2, 2, 1, '2024-11-02', 1),
-(8, 2, 2, 2, '2024-11-03', 2),
-(9, 2, 4, 1, '2024-11-03', 2),
-(10, 2, 4, 2, '2024-11-08', 3),
-(11, 2, 3, 1, '2024-11-03', 2),
-(12, 3, 1, 1, '2024-11-06', 1);
+(12, 3, 1, 1, '2024-11-06', 1),
+(13, 3, 1, 2, '2024-11-23', 1),
+(14, 3, 2, 1, '2024-11-13', 1),
+(15, 3, 5, 1, '2024-11-28', 1),
+(17, 3, 1, 3, '2024-11-08', 1),
+(18, 1, 1, 1, '2024-11-08', 1),
+(19, 1, 3, 2, '2024-11-07', 1),
+(20, 3, 4, 1, '2024-11-08', 1),
+(21, 3, 4, 2, '2024-11-16', 1),
+(25, 3, 6, 1, '2024-11-08', 1);
 
 -- --------------------------------------------------------
 
@@ -173,7 +172,7 @@ ALTER TABLE `vacuna_tipo`
 -- AUTO_INCREMENT de la tabla `niños`
 --
 ALTER TABLE `niños`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `personal`
@@ -185,7 +184,7 @@ ALTER TABLE `personal`
 -- AUTO_INCREMENT de la tabla `vacunaciones`
 --
 ALTER TABLE `vacunaciones`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `vacuna_tipo`
